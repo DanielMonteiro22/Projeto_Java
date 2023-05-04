d0a9d2805b172605c3f0df10ffa2e84b75a59c5f
DanielMonteiro22 <danielmontcosta55@gmail.com>
2023-05-03 21:03:18 -0300

    first commit

---
+++ src/test/java/com/Clientes/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Clientes.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
