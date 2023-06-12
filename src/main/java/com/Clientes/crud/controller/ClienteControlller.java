c5303e9729d1e96ae49081b8321d8148b214bf01
DanielMonteiro22 <danielmontcosta55@gmail.com>
2023-06-12 17:00:54 -0300

    dtos

--- src/main/java/com/Clientes/crud/controller/ClienteControlller.java
+++ src/main/java/com/Clientes/crud/controller/ClienteControlller.java
@@ -1,8 +1,26 @@
 package com.Clientes.crud.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.net.server.Client;
+import dto.ClientesDto;
 
 @RestController
 public class ClienteControlller {
+	
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientesDto> getClienteById(@PathVariable Long id) {
+       
+       
+        ClientesDto clienteDTO = new ClientesDto();
+       
+
+        return ResponseEntity.ok(clienteDTO);
+    }
+
 
 }
